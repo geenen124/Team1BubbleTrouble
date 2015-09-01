e2a7b3b60ab41a6214b4a3cada31fc381ae915df
Menno Oudshoorn <m.j.j.oudshoorn@student.tudelft.nl>
2015-09-01 19:02:45 +0200

    useless modify to test octopull

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -5,4 +5,8 @@ public class TestClass {
     public static void main(String[] args) {
         System.out.print("DADADADADADADA");
     }
+    
+    public static void thisHasNoJavaDoc() {
+    	System.out.print("It's not even called xD");
+    }
 }
