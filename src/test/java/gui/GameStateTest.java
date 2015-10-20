82a1c62f203965cd29aeb297c50560f67340ea51
Bart de Jonge <B.A.P.deJonge@student.tudelft.nl>
2015-10-20 20:37:13 +0200

    reversing previous tests

--- src/test/java/gui/GameStateTest.java
+++ src/test/java/gui/GameStateTest.java
@@ -7,26 +7,26 @@ import org.junit.Test;
 
 
 public class GameStateTest {
-//
-//	@Test
-//	public void testGetID() {
-//		MainGame mg = new MainGame(null);
-//		GameState gamestate = new GameState(mg);
-//		assertEquals(mg.getGameState(), gamestate.getID());
-//	}
 
-//	@Test
-//	public void testGameState() {
-//		MainGame mg = new MainGame(null);
-//		GameState gamestate = new GameState(mg);
-//		assertEquals(gamestate.getmainGame(),mg);
-//	}
-//	
-//	@Test
-//	public void testGetMainGame() {
-//		MainGame mg = new MainGame(null);
-//		GameState gamestate = new GameState(mg);
-//		assertEquals(mg,gamestate.getmainGame());
-//	}
+	@Test
+	public void testGetID() {
+		MainGame mg = new MainGame(null);
+		GameState gamestate = new GameState(mg);
+		assertEquals(mg.getGameState(), gamestate.getID());
+	}
+
+	@Test
+	public void testGameState() {
+		MainGame mg = new MainGame(null);
+		GameState gamestate = new GameState(mg);
+		assertEquals(gamestate.getmainGame(),mg);
+	}
+	
+	@Test
+	public void testGetMainGame() {
+		MainGame mg = new MainGame(null);
+		GameState gamestate = new GameState(mg);
+		assertEquals(mg,gamestate.getmainGame());
+	}
 
 }
