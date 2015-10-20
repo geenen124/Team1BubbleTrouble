7158fc1cab22d3d48404c52515161f5e630f8f24
Menno Oudshoorn <m.j.j.oudshoorn@student.tudelft.nl>
2015-10-20 13:07:54 +0200

    Created LevelContainerIterator <organisation

---
+++ src/main/java/iterator/LevelContainerIterator.java
@@ -0,0 +1,50 @@
+package iterator;
+
+import java.util.ArrayList;
+
+import levels.Level;
+
+/**
+ * Iterator for CircleList.
+ * @author Menno, Stefan
+ *
+ */
+public class LevelContainerIterator implements Iterator {
+	private ArrayList<Level> list;
+	private int position;
+	
+	/**
+	 * Construct a new CoinListIterator.
+	 * @param list the list with circles to iterate over.
+	 */
+	public LevelContainerIterator(ArrayList<Level> list) {
+		this.list = list;
+		reset();
+	}
+	
+	/**
+	 * Reset the position.
+	 */
+	private void reset() {
+		position = 0;
+	}
+
+
+	@Override
+	public boolean hasNext() {
+		return position < list.size();
+	}
+
+	@Override
+	public Object next() {
+		return list.get(position - 1);
+		
+	}
+
+	@Override
+	public void remove() {
+		list.remove(position - 1);
+		position--;
+	}
+
+}
