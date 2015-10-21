31a8e16d30342d424887d53f42011ad725c7ca5f
Mark van de Ruit <markvanderuit@gmail.com>
2015-10-21 17:43:09 +0200

    Small change to test.
    <testing

--- src/test/java/gui/ElementTest.java
+++ src/test/java/gui/ElementTest.java
@@ -1,6 +1,7 @@
 package gui;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import guiobjects.Button;
